85bbe7dbdb6165b47bbc45fb40a7b19663bce6d4
JoelNamaZuzzi <59820667+JoelNamaZuzzi@users.noreply.github.com>
2022-10-28 16:07:56 +0200

    Use event triggers instead of onClick()
    Switched to a system that uses Events trigger to correctly register hold (not nice but working)

--- Assets/Scripts/MoveNActiveGO.cs
+++ Assets/Scripts/MoveNActiveGO.cs
@@ -7,30 +7,74 @@ public class MoveNActiveGO : MonoBehaviour
     public GameObject objToModifiy;
     public Vector3 Min;
     public Vector3 Max;
-
-    public void move(Vector3 mov){
+    [Range (0f, 0.1f)]
+    public float speed;
+    private bool Front=false;
+    private bool Back=false;
+    private bool Left=false;
+    private bool Right=false;
+    private bool Up=false;
+    private bool Down=false;
+    private bool RotLeft=false;
+    private bool RotRight=false;
+    private void move(Vector3 mov){
         objToModifiy.transform.position =  Clamp(objToModifiy.transform.position + mov);
     }
-
-    public void moveFront(){
-        move(new Vector3(0,0,-0.1f));
-    }
-    public void moveBack(){
-        move(new Vector3(0,0,0.1f));
-    }
-    public void moveLeft(){
-        move(new Vector3(0.1f,0,0));
-    }
-    public void moveRight(){
-        move(new Vector3(-0.1f,0,0));
-    }
-    public void moveUp(){
-        move(new Vector3(0,0.1f,0));
-    }
-    public void moveDown(){
-        move(new Vector3(0,-0.1f,0));
+    private void rotate(float x, float y, float z){
+        objToModifiy.transform.Rotate(x,y,z);
     }
 
+    public void moveFront(bool setbool){
+        Front = setbool;
+    }
+    public void moveBack(bool setbool){
+        Back = setbool;
+    }
+    public void moveLeft(bool setbool){
+        Left = setbool;
+    }
+    public void moveRight(bool setbool){
+        Right = setbool;
+    }
+    public void moveUp(bool setbool){
+        Up = setbool;
+    }
+    public void moveDown(bool setbool){
+        Down = setbool;
+    }
+    public void RotateLeft(bool setbool){
+        RotLeft = setbool;
+
+    }
+    public void RotateRight(bool setbool){
+        RotRight = setbool;
+    }
+    private void FixedUpdate() {
+        if(Front){
+            move(new Vector3(0,0,-speed));
+        }
+        if(Back){
+            move(new Vector3(0,0,speed));
+        }
+        if(Left){
+            move(new Vector3(speed,0,0));
+        }
+        if(Right){
+            move(new Vector3(-speed,0,0));
+        }
+        if(Up){
+            move(new Vector3(0,speed,0));
+        }
+        if(Down){
+            move(new Vector3(0,-speed,0));
+        }
+        if(RotLeft){
+            rotate(0,speed,0);
+        }
+        if(RotRight){
+            rotate(0,-speed,0);
+        }
+    }
     private Vector3 Clamp(Vector3 vec){
         vec.x = Mathf.Clamp(vec.x, Min.x, Max.x);
         vec.y = Mathf.Clamp(vec.y, Min.y, Max.y);
